d922a55641b63d966927f6cace6015dc7cc96a58
morozovavv <morozovavv@byterg.ru>
2023-10-11 14:21:56 +0300

    deleted extra fields

--- main1.c
+++ main1.c
@@ -4,17 +4,11 @@ static GMainLoop *loop;
 
 typedef struct PipelineData {
 	GstElement	*pipeline;
-	GstElement *file;
 	GstElement *bin;
 	GstElement *conv;
-	GstElement *file1;
 	GstElement *bin1;
 	GstElement *conv1;
-	GstElement *capsfilter1;
 	GstElement *sink;
-	GstElement *rate1;
-	GstElement *queue1;
-	GstElement	*rtsp;
 } PipeData;
 
 gboolean is_source1_active = 1;
@@ -91,15 +85,13 @@ int main(int argc, char **argv) {
 	GstMessage *msg;
 	int			switch_time = 0;
 	gboolean terminate = FALSE;
-	GstCaps *caps;
+	
 	//проверка на правильность агрументов
 	//адрес сервера, путь к файлу, интервал переключения
 
-	// gst_caps_set_value(caps, "framerate", "30/1");
 	gst_init(&argc, &argv);
 
-	caps = gst_caps_new_simple ("video/x-raw",
-      "framerate", GST_TYPE_FRACTION, 3, 1, NULL);
+
 	if (argc < 4) {
 		printf("Недостаточно аргументов.\n"); //стоит написать в каком порядке?
 		exit(0);
@@ -113,16 +105,12 @@ int main(int argc, char **argv) {
 		printf("no pipe created\n");
 		return 1;
 	}
-	data.rtsp = gst_element_factory_make("rtspsrc", "rtsp_src");
-	data.file = gst_element_factory_make("filesrc", "file_src");
-	data.file1 = gst_element_factory_make("filesrc", "file_src_1");
-	data.bin = gst_element_factory_make("decodebin", "decode_bin");
+	data.bin = gst_element_factory_make("uridecodebin", "uridecode_bin");
 	data.bin1 = gst_element_factory_make("uridecodebin", "uridecode_bin1");
 	data.sink = gst_element_factory_make("xvimagesink", "sink"); 
 	data.conv = gst_element_factory_make("autovideoconvert", "conv");
 	data.conv1 = gst_element_factory_make("autovideoconvert", "conv1");
-	data.queue1 = gst_element_factory_make("queue", "queue1");
-	if (!data.pipeline || !data.file || !data.file1 || !data.bin || !data.conv || !data.bin1 || !data.conv1 || !data.sink|| !data.queue1 || !data.rtsp) {
+	if (!data.pipeline || !data.bin || !data.conv || !data.bin1 || !data.conv1 || !data.sink) {
         g_print("One or more elements could not be created. Exiting.\n");
         return -1;
     }
@@ -140,39 +128,24 @@ int main(int argc, char **argv) {
     strcpy(uri, "file://");
     strcat(uri, path);
     GST_ERROR("uri = %s\n", uri);
-	gst_util_set_object_arg (G_OBJECT(data.file), "location", argv[1]);
-	// gst_util_set_object_arg (G_OBJECT(data.rtsp), "location", argv[2]);
-	gst_util_set_object_arg (G_OBJECT(data.file1), "location", argv[1]);
-	gst_util_set_object_arg (G_OBJECT(data.file1), "do-timestamp ", "true");
-
+	
     gst_util_set_object_arg (G_OBJECT(data.bin1), "uri", uri);
+    gst_util_set_object_arg (G_OBJECT(data.bin), "uri", argv[1]);
 	printf("arguments are set\n");
 
-	gst_bin_add_many(GST_BIN(data.pipeline), data.file1, data.bin1, data.conv1, data.sink, NULL);
+	gst_bin_add_many(GST_BIN(data.pipeline), data.bin1, data.conv1, data.sink, NULL);
 	GST_ERROR("elements added to bin\n");
 
 
 	if (is_source1_active) {
 		// if (!gst_element_link(data.file1, data.bin1))
 			GST_ERROR("no link 12");
-		// GstPad* rate_src_pad = gst_element_get_static_pad(data.rate1, "src");
-		// // gst_pad_push_event (rate_src_pad, gst_event_new_caps (caps));
-		// gst_pad_set_caps(rate_src_pad, caps);
-
-		// if (!gst_element_link(data.queue1, data.bin1))
-			// GST_ERROR("no link 22");
-
-		// if (!gst_element_link(data.capsfilter1, data.bin1))
-		// 	GST_ERROR("no link 34");
-
+		
 		if (!gst_element_link(data.conv1, data.sink))
 			GST_ERROR("no link 45");
 		g_signal_connect (data.bin1, "pad-added", G_CALLBACK (pad_added_handler), &data);
-		gst_element_set_state(data.file, GST_STATE_NULL);
 		gst_element_set_state(data.bin, GST_STATE_NULL);
 		gst_element_set_state(data.conv, GST_STATE_NULL);
-		// gst_element_set_state(data.file1, GST_STATE_PLAYING);
-		// // // gst_element_set_state(data.rtsp, GST_STATE_PLAYING);
 		gst_element_set_state(data.bin1, GST_STATE_PLAYING);
 		gst_element_set_state(data.conv1, GST_STATE_PLAYING);
 		// gst_element_set_state(data.sink, GST_STATE_PLAYING);
@@ -183,23 +156,12 @@ int main(int argc, char **argv) {
 		gst_element_set_state(data.bin1, GST_STATE_PLAYING);
         GstPad* bin1_src_pad = gst_element_get_static_pad(data.bin1, "src");
 		gst_pad_activate_mode (bin1_src_pad, GST_PAD_MODE_PUSH, TRUE);
-        // gst_element_set_state(data.pipeline, GST_STATE_PLAYING);
-		// while (!gst_element_link(data.bin1, data.conv1)) {
-		// 	GST_WARNING("no link 23");
-		// 	g_usleep(500000);
-        //     gst_element_set_state(data.pipeline, GST_STATE_PLAYING);
-		// }
-		gst_element_link(data.bin1, data.conv1);
+        gst_element_link(data.bin1, data.conv1);
         gst_element_set_state(data.pipeline, GST_STATE_PLAYING);
 		// gst_element_link(data.bin1, data.capsfilter1);
 		// gst_element_link(data.bin1, data.conv1);
 	    GST_DEBUG_BIN_TO_DOT_FILE(data.pipeline, GST_DEBUG_GRAPH_SHOW_ALL, "pipeline");
-		// gst_element_set_state(data.file1, GST_STATE_PLAYING);
-		// GstPad* src_pad = gst_element_get_static_pad(data.file1, "src");
-		// gst_pad_activate_mode (src_pad, GST_PAD_MODE_PUSH, TRUE);
-		// GstPad* bin1_src_pad = gst_element_get_static_pad(data.bin1, "src");
-		// gst_pad_activate_mode (bin1_src_pad, GST_PAD_MODE_PUSH, TRUE);
-	}
+		}
 	GST_WARNING("Running ...\n");
 	loop = g_main_loop_new(NULL, FALSE);
     g_main_loop_run(loop);
